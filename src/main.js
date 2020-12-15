997b0ddb76d3e9f7443a563ed666438dc329dedf
杜荣林 <1024355946@qq.com>
2020-12-15 19:51:52 +0800

    项目搭建，初始化脚手架

--- src/main.js
+++ src/main.js
@@ -5,8 +5,24 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+
+// van-ui
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+
+// axios 配置
+import axios from'./utils/request.js'
+Vue.prototype.$axios = axios
+
+// element ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// import App from './App.vue';
+
+Vue.use(ElementUI);
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App)
 }).$mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div>
+		<van-button type="primary" @click="add">立即登录</van-button>
+	</div>
 </template>
+
+<script>
+	export default {
+		data() {
+			return {
+			value:''
+			};
+		},
+		created() {
+
+		},
+		mounted() {
+			this.$axios.post('https://www.liulongbin.top:8888/api/private/v1/login',{username:'admin',password:'123456'}).then(res=>{
+				console.log(res)
+			})
+		},
+		methods: {
+		add(){
+			this.$toast.success('登录成功')
+		}
+		},
+		computed: {
+
+		}
+	};
+</script>
+
+<style scoped>
+
+</style>
